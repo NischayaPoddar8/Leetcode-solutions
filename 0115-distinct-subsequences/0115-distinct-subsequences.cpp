887324b3cdced4866385d92ba3d280ed9cdcff64
Nischaya Poddar <nischayapoddar8@gmail.com>
2026-09-08 17:48:05 +0530

    Time: 30 ms (54.68%), Space: 44.2 MB (29.5%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -24,7 +24,26 @@ public:
     int numDistinct(string s, string t) {
         int m = s.size();
         int n = t.size();
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return lcs(s,t,m-1,n-1,dp);
+        vector<vector<unsigned long long>>dp(m+1,vector<unsigned long long>(n+1,0));
+
+        for(int i=0;i<=m;i++){
+            for(int j=0;j<=n;j++){
+
+                if(j==0) dp[i][j] = 1;
+                if(j>0 && i==0) dp[i][j] = 0;
+            } 
+        }
+
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+
+                unsigned long long pick = 0;
+                unsigned long long notPick = dp[i-1][j];
+
+                if(s[i-1]==t[j-1]) pick = dp[i-1][j-1];
+                dp[i][j] = pick+notPick;
+            }
+        }
+        return dp[m][n];
     }
 };
