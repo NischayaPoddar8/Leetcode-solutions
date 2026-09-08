02b7fe0c5672b1948559e71657d2195c40025397
Nischaya Poddar <nischayapoddar8@gmail.com>
2026-09-08 17:36:09 +0530

    Time: 35 ms (30.62%), Space: 26.9 MB (68.75%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -2,9 +2,8 @@ class Solution {
 
     int lcs(string &s1,string &s2,int i,int j,vector<vector<int>>&dp){
 
-        if(i<0 && j>=0) return 0;
-        if(j<0 && i>=0) return 1;
-        if(j==0 && i==0) if(s1[i]==s2[j]) return 1;
+        if(j<0) return 1; // j reached before i
+        if(i<0) return 0;
 
         if(dp[i][j]!=-1) return dp[i][j];
 
