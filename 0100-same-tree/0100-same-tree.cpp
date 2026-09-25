8ca885364924d360123bd7de5f2103bfde0daae7
Nischaya Poddar <nischayapoddar8@gmail.com>
2026-09-25 10:36:17 +0530

    Time: 0 ms (100%), Space: 12.8 MB (46.88%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -10,21 +10,43 @@
  * };
  */
 class Solution {
-
-    bool dfs(TreeNode* p,TreeNode* q){
-
-        if(p==NULL && q==NULL) return true;
-        if((!p && q)|| (p && !q)) return false;
-        if(p->val!=q->val) return false;
-
-        bool leftPart = dfs(p->left,q->left);
-        bool rightPart = dfs(p->right,q->right);
-
-        return (leftPart && rightPart);
-    }
-
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        return dfs(p,q);
+
+        if(!p && !q) return true;
+        if((p && !q) || (!p && q) ||(p->val!=q->val)) return false;
+
+        queue<pair<TreeNode*,TreeNode*>>qu;
+        qu.push({p,q});
+
+        while(!qu.empty()){
+
+            int size = qu.size();
+
+            for(int i=0;i<size;i++){
+                
+                TreeNode* node1 = qu.front().first;
+                TreeNode* node2 = qu.front().second;
+                qu.pop();
+
+                if((node1->left && !node2->left) || (!node1->left && node2->left)) return false;
+
+                if(node1->left && node2->left) {
+                    if(node1->left->val != node2->left->val) return false;
+                    qu.push({node1->left,node2->left});
+                } 
+
+                if((node1->right && !node2->right) || (!node1->right && node2->right)) return false;
+
+                if(node1->right && node2->right) {
+                    if(node1->right->val != node2->right->val) return false;
+
+                    qu.push({node1->right,node2->right});
+                }
+                
+            }   
+        }
+        
+        return true;
     }
 };
